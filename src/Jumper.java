44030d85c64dddafec2623cd9caf34d4b08a4a1f
kqwyf <kqwyfhit@163.com>
2018-03-02 19:41:24 +0800

    finished.

--- src/Jumper.java
+++ src/Jumper.java
@@ -3,6 +3,7 @@ import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Dimension;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -23,6 +24,8 @@ public class Jumper
 	JFrame mainFrame;
 	Controller mainView;
 
+	private int score;
+
 	public static void main(String[] args)
 	{
 		Jumper jumper=new Jumper();
@@ -31,7 +34,7 @@ public class Jumper
 	public Jumper()
 	{
 		mainFrame=new JFrame(APP_NAME);
-		mainView=new Controller((Integer score)->setScore(score));
+		mainView=new Controller((Integer score)->addScore(score));
 		mainFrame.setSize(new Dimension(WIDTH,HEIGHT));
 		mainFrame.setResizable(false);
 		mainFrame.setLocationRelativeTo(null);
@@ -39,10 +42,14 @@ public class Jumper
 		mainFrame.getContentPane().add(mainView,BorderLayout.CENTER);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainFrame.setVisible(true);
+		score=0;
+		mainView.initialize();
 	}
 
-	private void setScore(int score)
+	private void addScore(int s)
 	{
+		if(s<0) score=0;
+		else score+=s;
 		mainFrame.setTitle(APP_NAME+" - Score:"+score);
 	}
 }
@@ -52,9 +59,9 @@ class Player
 	public int x,y,width,height,h;
 	private int v;
 	
-	public static final int FLYING_SPEED=2;
-	private static final double PRESS_SPEED=1.1;
-	private static final int SPEED_K=5;
+	public static final int FLYING_SPEED=5;
+	private static final int SPEED_K=10;
+	private static final int ACCELERATION=1;
 
 	public Player(int x,int y,int r)
 	{
@@ -77,36 +84,40 @@ class Player
 
 	public boolean press()
 	{
+		if(height<=width/3) return true;
 		int old_height=height;
-		height=(int)(height/PRESS_SPEED+0.5);
+		v+=ACCELERATION;
+		height=width-v/SPEED_K;
 		y-=height-old_height;
-		return height==old_height;
+		return false;
 	}
 
 	public void bounce()
 	{
-		v=SPEED_K*(height-width);
 		y-=width-height;
 		height=width;
+		h=v;
+		//System.out.println("h="+h+"\tv="+v);
 	}
 
 	public boolean step(int direction)
 	{
 		if(h<=0) return true;
+		v-=Controller.GRAVITY;
 		h+=v;
-		if(h<0)
+		if(h<=0)
 		{
 			h=0;
 			v=0;
 		}
-		v-=Controller.GRAVITY;
+		//System.out.println("h="+h+"\tv="+v);
 		return false;
 	}
 
 	public void draw(Graphics g)
 	{
 		g.setColor(Color.red);
-		g.fillOval(x,y-h,width,height);
+		g.fillOval(x,y-h/SPEED_K,width,height);
 	}
 }
 
@@ -114,8 +125,8 @@ class Box
 {
 	public int x,y,r;
 
-	public static final int MIN_R=10;
-	public static final int MAX_R=20;
+	public static final int MIN_R=25;
+	public static final int MAX_R=35;
 	public static final int AVE_R=(MIN_R+MAX_R)/2;
 
 	private static int reward=1;
@@ -166,21 +177,22 @@ class Box
 		g.setColor(Color.blue);
 		g.drawLine(x,getCenterY(),getCenterX(),y);
 		g.drawLine(x+2*r,getCenterY(),getCenterX(),y);
-		g.drawLine(x,getCenterY(),getCenterX(),(int)(y+r*Controller.T));
-		g.drawLine(x+2*r,getCenterY(),getCenterX(),(int)(y+r*Controller.T));
+		g.drawLine(x,getCenterY(),getCenterX(),(int)(y+2*r*Controller.T));
+		g.drawLine(x+2*r,getCenterY(),getCenterX(),(int)(y+2*r*Controller.T));
 	}
 }
 
 class Controller extends JPanel
 {
-	public static final int GRAVITY=1;
+	public static final int GRAVITY=3;
 	public static final double T=0.5; //tan
 
 	private static final int PLAYER_R=10;
 	private static final int PLAYER_X=(Jumper.WIDTH-PLAYER_R)/2;
 	private static final int PLAYER_Y=(int)(Jumper.HEIGHT*0.618-PLAYER_R/2);
-	private static final int MIN_DISTANCE=10;
-	private static final int MAX_DISTANCE=30;
+	private static final int MIN_DISTANCE=50;
+	private static final int MAX_DISTANCE=100;
+	private static final int INTERVAL=20;
 
 	private Player player;
 	private ArrayList<Box> boxes;
@@ -200,6 +212,7 @@ class Controller extends JPanel
 				{
 					public void mousePressed(MouseEvent e)
 					{
+						requestFocus();
 						if(playing&&!moving&&e.getButton()==e.BUTTON1)
 							pressing=true;
 					}
@@ -218,13 +231,16 @@ class Controller extends JPanel
 				{
 					public void keyPressed(KeyEvent e)
 					{
+						//System.out.println("playing:"+playing+"\tkeyCode="+e.getKeyCode());
 						if(!playing&&e.getKeyCode()==KeyEvent.VK_ENTER)
 						{
-							playing=true;
+							//System.out.println("restart.");
+							timer.stop();
+							initialize();
 						}
 					}
 				});
-		timer=new Timer(20,new ActionListener()
+		timer=new Timer(INTERVAL,new ActionListener()
 				{
 					public void actionPerformed(ActionEvent e)
 					{
@@ -235,42 +251,50 @@ class Controller extends JPanel
 						}
 					}
 				});
-		initialize();
-		Box.initialize();
 	}
 
 	public void updateView()
 	{
 		Graphics g=this.getGraphics();
 		g.clearRect(0,0,Jumper.WIDTH,Jumper.HEIGHT);
-		if(!failed) player.draw(g);
 		for(Box box:boxes)
 		{
-			if(box.x>WIDTH||box.x+box.r<0||box.y>HEIGHT)
-				;//boxes.remove(box);
-			else
+			/*if(box.x>WIDTH||box.x+box.r<0||box.y>HEIGHT)
+				boxes.remove(box);
+			else*/
 				box.draw(g);
+		}
+		if(!failed) player.draw(g);
+		else
+		{
+			//System.out.println("failed.");
+			g.setColor(Color.black);
+			g.setFont(new Font("Consolas",Font.PLAIN,15));
+			g.drawString("Press Enter to restart.",2,15);
 		}
 	}
 
-	private void initialize()
+	public void initialize()
 	{
 		playing=true;
 		pressing=false;
 		moving=false;
+		failed=false;
+		scoreController.accept(-1);
 		direction=1;
 		player=new Player(PLAYER_X,PLAYER_Y,PLAYER_R);
+		Box.initialize();
 		boxes=new ArrayList<Box>();
-		boxes.add(new Box(player.x-Box.AVE_R,(int)(player.y-Box.AVE_R*T),Box.AVE_R));
+		boxes.add(new Box(player.getCenterX()-Box.AVE_R,(int)(player.getCenterY()-Box.AVE_R*T),Box.AVE_R));
 		boxes.add(generateBox());
 		timer.start();
 	}
 
 	private void step()
 	{
+		//System.out.println("player=("+player.x+","+player.y+")");
 		if(failed)
 		{
-			getGraphics().drawString("Press Enter to restart.",0,0);
 			playing=false;
 		}
 		else if(pressing)
@@ -286,7 +310,7 @@ class Controller extends JPanel
 				if(score>0)
 				{
 					scoreController.accept(score);
-					generateBox();
+					boxes.add(generateBox());
 					return;
 				}
 				boolean flag=false;
@@ -315,9 +339,9 @@ class Controller extends JPanel
 	{
 		direction=(int)(Math.random()*2)*2-1; //get a random number either 1 or -1
 		int distance=(int)(Math.random()*(MAX_DISTANCE-MIN_DISTANCE)+MIN_DISTANCE);
-		System.out.println(distance);
-		int centerX=player.x+direction*distance;
-		int centerY=(int)(player.y-distance*T);
+		//System.out.println(distance);
+		int centerX=player.getCenterX()+direction*distance;
+		int centerY=(int)(player.getCenterY()-distance*T);
 		int r=(int)(Math.random()*(Box.MAX_R-Box.MIN_R)+Box.MIN_R);
 		return new Box(centerX-r,(int)(centerY-r*T),r);
 	}
